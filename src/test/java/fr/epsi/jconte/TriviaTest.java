604bf111c5e840a9f20b5e078b5ac9e4e2f82076
joconte <etud@contejonathan.com>
2019-11-26 14:51:27 +0100

    first commit with test

---
+++ src/test/java/fr/epsi/jconte/TriviaTest.java
@@ -0,0 +1,36 @@
+package fr.epsi.jconte;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriviaTest {
+
+    @Test
+    public void createRockQuestion() {
+    }
+
+    @Test
+    public void isPlayable() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void howManyPlayers() {
+    }
+
+    @Test
+    public void roll() {
+    }
+
+    @Test
+    public void wasCorrectlyAnswered() {
+    }
+
+    @Test
+    public void wrongAnswer() {
+    }
+}
